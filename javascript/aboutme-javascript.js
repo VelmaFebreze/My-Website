d3f1e47fb5d852476b3df99dd5958ec7b0782794
VelmaFebreze <velmafebreze99@gmail.com>
2023-11-03 11:00:37 -0700

    added Leaflet map

--- javascript/aboutme-javascript.js
+++ javascript/aboutme-javascript.js
@@ -19,3 +19,18 @@ const observer = new IntersectionObserver(addSlideIn, options)
 items.forEach(item => {
   observer.observe(item);
 })
+
+
+
+const map = L.map('map').setView([36.790508, //latitude
+        -81.770187], //longitude
+            12);    //zoom
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var circle = L.circle([36.790508, -81.770187], {radius: 1500, color: 'lime'}).addTo(map);
+
+        circle.bindPopup("The small town of Glade Spring, Virginia.");
